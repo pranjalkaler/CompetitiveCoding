0e069743eb444e81f7bab6cfecc9204dcc9b27be
Vishal Vishwanathan <vishal.viswanathan9@gmail.com>
2020-09-02 19:50:29 +0530

    Convert a sorted array to a balanced BST

---
+++ LeetCode/CPP/ConvertSortedArrayToBST.cpp
@@ -0,0 +1,44 @@
+// Leetcode : https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+// Author : Vishal Vishwanathan
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    TreeNode* util(std::vector<int> nums, int start, int end ){
+        if(start<=end){
+            int mid = start + (end-start)/2;
+            TreeNode* x = new TreeNode(nums[mid]);
+            x->left = util(nums, start,mid-1);
+            x->right = util(nums, mid+1,end);
+            return x;
+        }
+        return nullptr;
+    }
+    public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+        return util(nums,0,nums.size()-1);
+    }
+};
+
+
+int main() {
+
+    Solution s;
+    std::vector<int> v = {10,-3,0,5,9};
+    TreeNode* root = s.sortedArrayToBST(v);
+    return 0;
+}
+
